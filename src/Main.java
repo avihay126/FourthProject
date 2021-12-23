5d84fe4119c4be20674538271e2c54855f6b33ef
Avihay Swissa <avihay126@gmail.com>
2021-12-23 17:29:35 +0200

    delete

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("blah blah bl;ah");
-        System.out.println("commit from shir elfasi - please delete");
         chooseOption();
-
-
     }
 
     public static void printMenu() {
